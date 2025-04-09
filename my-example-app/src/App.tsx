23f15b3bdf36137553c12c15a7925eaf8e8c2990
Nhat Nnguyen <nhat.nguyen@asnet.com.vn>
2025-04-09 14:22:13 +0700

    feat(): create example with display data

--- my-example-app/src/App.tsx
+++ my-example-app/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import MyButton from './components/button'
+import Profile from './components/profile'
 
 function App() {
   return (
     <>
     <h1>Welcome to my app</h1>
     <MyButton />
+    <Profile />
     </>
   )
 }
---
+++ my-example-app/src/components/profile.tsx
@@ -0,0 +1,24 @@
+const user = {
+  name: 'John Doe',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+}
+
+const Profile = () => {
+  return (
+    <>
+      <h1>{user.name}</h1>
+      <img 
+        src={user.imageUrl} 
+        alt={"Photo of " + user.name} 
+        className="avatar" 
+        style={{ 
+          width: user.imageSize, 
+          height: user.imageSize 
+        }} 
+      />
+    </>
+  );
+}
+
+export default Profile;

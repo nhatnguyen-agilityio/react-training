d7d4deeb6f1bf459f72c09cdd97f46006b7a52ff
Nhat Nnguyen <nhat.nguyen@asnet.com.vn>
2025-04-14 16:21:04 +0700

    feat(): use confitional to render component

--- my-example-app/src/components/inspirationGenerator.tsx
+++ my-example-app/src/components/inspirationGenerator.tsx
@@ -1,16 +1,19 @@
 import { useState } from "react";
 import quotes from "../mock/quotes";
 import FancyText from "./fancyText";
+import inspirations from "../mock/inspirations";
+import Color from "./color";
 
 export default function InspirationGenerator({ children }: Readonly<{ children: React.ReactNode }>) {
     const [index, setIndex] = useState(0);
     const quote = quotes[index]
-    const next = () => setIndex((index + 1) % quotes.length);
+    const inspiration = inspirations[index]
+    const next = () => setIndex((index + 1) % inspirations.length);
 
     return (
         <>
-            <p>Your inspiration quote is:</p>
-            <FancyText text={quote}/>
+            <p>Your inspiration {inspiration.type} is:</p>
+            {inspiration.type === 'quote' ? <FancyText text={inspiration.value} /> : <Color color={inspiration.value} />}
             <button onClick={next}>Inspire me again</button>
             {children}
         </>

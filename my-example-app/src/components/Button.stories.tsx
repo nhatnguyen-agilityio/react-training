0ae7af5ae4de14e3516ca3a48b7e09d93a770b70
Nhat Nnguyen <nhat.nguyen@asnet.com.vn>
2025-04-15 16:05:18 +0700

    feat(): create the simple storybook

---
+++ my-example-app/src/components/Button.stories.tsx
@@ -0,0 +1,34 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Button } from './button.tsx';
+
+const meta: Meta<typeof Button> = {
+  title: 'MyComponents/Button',
+  component: Button,
+  tags: ['autodocs'],
+  argTypes: {
+    onClick: { action: 'clicked' },
+    variant: {
+      control: {
+        type: 'radio',
+        options: ['primary', 'secondary'],
+      },
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Button>;
+export const Primary: Story = {
+  args: {
+    label: 'Primary Button',
+    variant: 'primary',
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    label: 'Secondary Button',
+    variant: 'secondary',
+  },
+};

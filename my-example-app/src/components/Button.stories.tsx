001de1427b2144837c6a3b7f7af42359dc461875
Nhat Nnguyen <nhat.nguyen@asnet.com.vn>
2025-04-15 21:22:49 +0700

    feat(): example with play function

--- my-example-app/src/components/Button.stories.tsx
+++ my-example-app/src/components/Button.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import { userEvent, within } from '@storybook/test';
 import { Button } from './button.tsx';
 
 const meta: Meta<typeof Button> = {
@@ -35,6 +36,11 @@ export const Primary: Story = {
     backgrounds: {
       default: 'dark',
     },
+  },
+  play: async ({ args, canvasElement }) => {
+    const canvas = within(canvasElement);
+    const button = await canvas.findByText(args.label);
+    await userEvent.click(button);
   },
 };
 

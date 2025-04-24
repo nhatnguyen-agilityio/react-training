3d1d2ca69b9d52d8d99779b84392fe1200264043
Nhat Nnguyen <nhat.nguyen@asnet.com.vn>
2025-04-24 16:16:43 +0700

    feat(): create the list of partner icons and map to company component

--- my-example-app/src/components/Miro/Body/Partner/index.tsx
+++ my-example-app/src/components/Miro/Body/Partner/index.tsx
@@ -5,16 +5,22 @@ import deloitte from "../../assets/deloitte.png";
 import walmart from "../../assets/walmart.png";
 import okta from "../../assets/okta.png";
 
+const partnerIcons = [
+  {id: 1, src: walmart},
+  {id: 2, src: cisco},
+  {id: 3, src: volvo},
+  {id: 4, src: deloitte},
+  {id: 5, src: okta},
+]
+
 const Partner = () => {
   return (
     <div className="my-[50px] w-4/5 mx-auto pb-[10px]">
       <h5 className="pt-[20px] font-light text-[18px]">Trusted by 45M+ users</h5>
       <div className="w-full h-[80px] flex mt-[10px]">
-        <Company img={walmart} />
-        <Company img={cisco} />
-        <Company img={volvo} />
-        <Company img={deloitte} />
-        <Company img={okta} />
+        {partnerIcons.map((item) => (
+          <Company key={item.id} img={item.src} />
+        ))}
       </div>
     </div>
   );

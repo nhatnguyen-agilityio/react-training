e4f14940202e89b5c2c4dc67cf04b41fdd40c8a2
Nhat Nnguyen <nhat.nguyen@asnet.com.vn>
2025-04-25 13:53:05 +0700

    feat(0: implement work together section

---
+++ my-example-app/src/components/Miro/Body/Introduction/IntroductionImage/index.tsx
@@ -0,0 +1,11 @@
+import Image from "../../../Common/image";
+
+const IntroductionImage = ({ blockClass, src, alt }: { blockClass: string, src: string, alt: string }) => {
+  return (
+    <div className={blockClass}>
+      <Image src={src} alt={alt} className="w-auto h-auto" />
+    </div>
+  )
+};
+
+export default IntroductionImage;
---
+++ my-example-app/src/components/Miro/Body/WorkTogether/index.tsx
@@ -0,0 +1,24 @@
+import IntroductionContent from "../Introduction/IntroductionContent";
+import IntroductionImage from "../Introduction/IntroductionImage";
+
+import hybridWorkImage from "../../assets/hybrid-work.png";
+
+const WorkTogether = () => {
+  return (
+    <div className="my-[70px] w-4/5 mx-auto flex justify-between">
+      <IntroductionContent
+        title={"Work together, wherever you work"}
+        description={"In the office, remote, or a mix of the two, with Miro, your team can connect, collaborate, and co-create in one space no matter where you are."}
+        link={"#"}
+        blockClass={"w-2/5 pr-[20px] flex flex-col justify-center"}
+      />
+      <IntroductionImage
+        blockClass={"w-1/2 h-full flex justify-end mr-[20px]"}
+        src={hybridWorkImage}
+        alt={"Hybrid Work"}
+      />
+    </div>
+  )
+}
+
+export default WorkTogether;

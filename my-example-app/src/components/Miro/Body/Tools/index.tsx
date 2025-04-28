f0686d2c5f6a1dfd6c4aa328f691ce329aff1efa
Nhat Nnguyen <nhat.nguyen@asnet.com.vn>
2025-04-28 11:20:12 +0700

    feat(): using react hooks to handle change content by each tag for tools section

--- my-example-app/src/components/Miro/Body/Tools/index.tsx
+++ my-example-app/src/components/Miro/Body/Tools/index.tsx
@@ -1,13 +1,15 @@
 import BuiltSection from "../BuiltSection";
 
+import bannerImage from "../../assets/tool-banner.png"
+
 const tags = [
-  { id: 1, text: "UX & Design", isActive: true },
-  { id: 2, text: "Marketing", isActive: false },
-  { id: 3, text: "Product Management", isActive: false },
-  { id: 4, text: "Engineering", isActive: false },
-  { id: 5, text: "Consultants", isActive: false },
-  { id: 6, text: "Agile Coaches", isActive: false },
-  { id: 7, text: "Sales", isActive: false },
+  { id: 1, title: "UX & Design", isActive: true, description: "(UX & Design) Unleash creative ideas and build on them with the help of sticky notes, images, mind maps, videos, drawing capabilities — the list goes on.", learnMoreUrl: "#", bannerImage: bannerImage },
+  { id: 2, title: "Marketing", isActive: false, description: "(Marketing) Unleash creative ideas and build on them with the help of sticky notes, images, mind maps, videos, drawing capabilities — the list goes on.", learnMoreUrl: "#", bannerImage: bannerImage },
+  { id: 3, title: "Product Management", isActive: false, description: "(Product Management) Unleash creative ideas and build on them with the help of sticky notes, images, mind maps, videos, drawing capabilities — the list goes on.", learnMoreUrl: "#", bannerImage: bannerImage },
+  { id: 4, title: "Engineering", isActive: false, description: "(Engineering) Unleash creative ideas and build on them with the help of sticky notes, images, mind maps, videos, drawing capabilities — the list goes on.", learnMoreUrl: "#", bannerImage: bannerImage },
+  { id: 5, title: "Consultants", isActive: false, description: "(Consultants) Unleash creative ideas and build on them with the help of sticky notes, images, mind maps, videos, drawing capabilities — the list goes on.", learnMoreUrl: "#", bannerImage: bannerImage },
+  { id: 6, title: "Agile Coaches", isActive: false, description: "(Agile Coaches) Unleash creative ideas and build on them with the help of sticky notes, images, mind maps, videos, drawing capabilities — the list goes on.", learnMoreUrl: "#", bannerImage: bannerImage },
+  { id: 7, title: "Sales", isActive: false, description: "(Sales) Unleash creative ideas and build on them with the help of sticky notes, images, mind maps, videos, drawing capabilities — the list goes on.", learnMoreUrl: "#", bannerImage: bannerImage },
 ]
 
 const Tools = () => {

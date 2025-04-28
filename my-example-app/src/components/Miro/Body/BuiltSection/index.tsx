2ab59cacd4e4336c7f63c72e066c13f413b2061b
Nhat Nnguyen <nhat.nguyen@asnet.com.vn>
2025-04-28 10:38:31 +0700

    feat(): update props type for built section component

--- my-example-app/src/components/Miro/Body/BuiltSection/index.tsx
+++ my-example-app/src/components/Miro/Body/BuiltSection/index.tsx
@@ -2,7 +2,7 @@ import Title from "../../Common/title";
 import BuiltBody from "./BuiltBody";
 import Tags from "./Tags";
 
-const BuiltSection = ({ tags, title }: { tags: { id: number, text: string }[], title: string }) => {
+const BuiltSection = ({ tags, title }: { tags: { id: number, text: string, isActive: boolean }[], title: string }) => {
   return (
     <div className="text-left w-4/5 mx-auto mb-[50px]">
       <Title title={title} />

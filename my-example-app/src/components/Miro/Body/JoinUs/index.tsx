5ac85f90f17d045ae4e0c0cb677a5dcc7107b447
Nhat Nnguyen <nhat.nguyen@asnet.com.vn>
2025-04-28 15:20:13 +0700

    feat(): handle event for button

--- my-example-app/src/components/Miro/Body/JoinUs/index.tsx
+++ my-example-app/src/components/Miro/Body/JoinUs/index.tsx
@@ -2,12 +2,16 @@ import Button from "../../Common/button";
 import Title from "../../Common/title";
 
 const JoinUs = () => {
+  const handleClick = () => {
+    alert("This feature will be available soon!");
+  }
+
   return (
     <div className="w-3/4 mb-[20px] flex flex-col justify-center items-center text-white mx-auto mt-[30px] h-[448px] bg-primary-100">
       <Title title={"Join 45M+ users today"} className="text-white leading-[56px] text-[48px]" />
       <p className="opacity-60 mb-[15px] mt-[7px]">Start for free — upgrade anytime.</p>
       <a className="opacity-60 underline hover:text-blue-200" href="#">Joining as an organization? Contact Sales</a>
-      <Button text="Sign up for free →" className="mt-[35px] mb-[5px] h-[51px] text-[18px] font-normal rounded-[32px] bg-button-100 hover:bg-blue-700 text-white py-2 px-7" />
+      <Button text="Sign up for free →" onClick={handleClick} className="mt-[35px] mb-[5px] h-[51px] text-[18px] font-normal rounded-[32px] bg-button-100 hover:bg-blue-700 text-white py-2 px-7" />
     </div>
   )
 };

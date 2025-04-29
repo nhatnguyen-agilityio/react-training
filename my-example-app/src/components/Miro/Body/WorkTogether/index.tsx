7c6cde395829e95220e651f83dd0abe5108fbcb4
Nhat Nnguyen <nhat.nguyen@asnet.com.vn>
2025-04-29 11:28:31 +0700

    feat(): update work together section to using rem instead pixel

--- my-example-app/src/components/Miro/Body/WorkTogether/index.tsx
+++ my-example-app/src/components/Miro/Body/WorkTogether/index.tsx
@@ -5,16 +5,16 @@ import hybridWorkImage from "../../assets/hybrid-work.png";
 
 const WorkTogether = () => {
   return (
-    <div className="my-[70px] w-4/5 mx-auto ">
+    <div className="my-17.5 w-4/5 mx-auto ">
       <div className="container flex justify-between">
         <IntroductionContent
           title={"Work together, wherever you work"}
           description={"In the office, remote, or a mix of the two, with Miro, your team can connect, collaborate, and co-create in one space no matter where you are."}
           link={"#"}
-          blockClass={"w-2/5 pr-[20px] flex flex-col justify-center"}
+          blockClass={"w-2/5 pr-5 flex flex-col justify-center"}
         />
         <IntroductionImage
-          blockClass={"w-1/2 h-full flex justify-end mr-[20px]"}
+          blockClass={"w-1/2 h-full flex justify-end mr-5"}
           src={hybridWorkImage}
           alt={"Hybrid Work"}
         />

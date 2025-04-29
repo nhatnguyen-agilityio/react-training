2e29cb9fd98d0aae761f54ba972644983459e535
Nhat Nnguyen <nhat.nguyen@asnet.com.vn>
2025-04-29 11:42:25 +0700

    feat(): update our works section to using rem instead pixel

--- my-example-app/src/components/Miro/Body/OurWorks/OurWorksTitle/index.tsx
+++ my-example-app/src/components/Miro/Body/OurWorks/OurWorksTitle/index.tsx
@@ -4,7 +4,7 @@ const OurWorksTitle = () => {
   return (
     <div className="w-1/2 flex flex-col text-center">
       <Title title={"Why companies large and small trust Miro"} />
-      <a href="#" className="mt-[10px] underline underline-offset-4 hover:text-blue-600">Contact Sales to request a demo</a>
+      <a href="#" className="mt-2.5 underline underline-offset-4 hover:text-blue-600">Contact Sales to request a demo</a>
     </div>
   );
 }
--- my-example-app/src/components/Miro/Body/OurWorks/index.tsx
+++ my-example-app/src/components/Miro/Body/OurWorks/index.tsx
@@ -3,9 +3,9 @@ import OurWorksTitle from "./OurWorksTitle";
 
 const OurWorks = () => {
   return (
-    <div className="w-full h-[923px] bg-[#FFD02F]">
+    <div className="w-full h-231 bg-[#FFD02F]">
       <div className="container flex items-center justify-center h-full">
-        <div className="w-4/5 mx-auto h-4/5 flex flex-col items-center">
+        <div className="mx-auto h-4/5 flex flex-col items-center">
           <OurWorksTitle />
           <About />
         </div>

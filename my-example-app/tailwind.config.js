dfc9ee2f448c46fa80ab8c3e71896cd2634c9754
Nhat Nnguyen <nhat.nguyen@asnet.com.vn>
2025-04-29 13:58:31 +0700

    feat(): update tailwind config file

--- my-example-app/tailwind.config.js
+++ my-example-app/tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js,jsx,ts,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx,html,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     container: {

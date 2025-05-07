98dac03af5168e5fe7c5fe2a1e187f0339b95ac6
Nhat Nnguyen <nhat.nguyen@asnet.com.vn>
2025-05-07 17:00:30 +0700

    feat(): apply grid layout for our works section

--- my-example-app/src/components/Miro/Body/OurWorks/index.tsx
+++ my-example-app/src/components/Miro/Body/OurWorks/index.tsx
@@ -3,7 +3,7 @@ import OurWorksTitle from "./OurWorksTitle";
 
 const OurWorks = () => {
   return (
-    <div className="w-full h-231 bg-[#FFD02F]">
+    <div className="h-231 bg-[#FFD02F]">
       <div className="container flex items-center justify-center h-full">
         <div className="mx-auto h-4/5 flex flex-col items-center">
           <OurWorksTitle />

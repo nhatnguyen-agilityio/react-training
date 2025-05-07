813302055df10f4e9c1a53ec433ea3db8e55514f
Nhat Nnguyen <nhat.nguyen@asnet.com.vn>
2025-05-07 16:43:52 +0700

    feat(): apply grid layout for conect tools section

--- my-example-app/src/components/Miro/Body/Introduction/IntroductionImage/index.tsx
+++ my-example-app/src/components/Miro/Body/Introduction/IntroductionImage/index.tsx
@@ -1,6 +1,6 @@
 import Image from "../../../Common/image";
 
-const IntroductionImage = ({ blockClass, src, alt }: { blockClass: string, src: string, alt: string }) => {
+const IntroductionImage = ({ blockClass, src, alt }: { blockClass?: string, src: string, alt: string }) => {
   return (
     <div className={blockClass}>
       <Image src={src} alt={alt} className="w-auto h-auto" />
--- my-example-app/src/components/Miro/Body/WorkTogether/index.tsx
+++ my-example-app/src/components/Miro/Body/WorkTogether/index.tsx
@@ -5,16 +5,16 @@ import hybridWorkImage from "../../assets/hybrid-work.png";
 
 const WorkTogether = () => {
   return (
-    <div className="my-18 w-4/5 mx-auto ">
-      <div className="container flex justify-between">
+    <div className="my-18">
+      <div className="container grid grid-cols-2">
         <IntroductionContent
           title={"Work together, wherever you work"}
           description={"In the office, remote, or a mix of the two, with Miro, your team can connect, collaborate, and co-create in one space no matter where you are."}
           link={"#"}
-          blockClass={"w-2/5 pr-5 flex flex-col justify-center"}
+          blockClass={"mr-15 flex flex-col justify-center"}
         />
         <IntroductionImage
-          blockClass={"w-1/2 h-full flex justify-end mr-5"}
+          blockClass={"h-full flex justify-end"}
           src={hybridWorkImage}
           alt={"Hybrid Work"}
         />

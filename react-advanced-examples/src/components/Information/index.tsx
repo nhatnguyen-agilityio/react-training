5e88b9bfa82947eae792103d0a0e2ab819c14092
Nhat Nnguyen <nhat.nguyen@asnet.com.vn>
2025-05-12 16:01:08 +0700

    feat(): create example with profiler

--- react-advanced-examples/src/components/Information/index.tsx
+++ react-advanced-examples/src/components/Information/index.tsx
@@ -1,5 +1,21 @@
-import { useState } from "react";
+import { Profiler, useState, type ProfilerOnRenderCallback } from "react";
 import Account from "./Account";
+
+const onRenderCallback: ProfilerOnRenderCallback = (
+  id,
+  phase,
+  actualDuration,
+  baseDuration,
+  startTime,
+  commitTime,
+) => {
+  console.log("🚀 ~ id:", id);
+  console.log("🚀 ~ phase:", phase);
+  console.log("🚀 ~ actualDuration:", actualDuration);
+  console.log("🚀 ~ baseDuration:", baseDuration);
+  console.log("🚀 ~ startTime:", startTime);
+  console.log("🚀 ~ commitTime:", commitTime);
+};
 
 const Information = () => {
   const [firstName, setFirstName] = useState("");
@@ -8,7 +24,7 @@ const Information = () => {
   const [password, setPassword] = useState("");
 
   return (
-    <>
+    <Profiler id="Information" onRender={onRenderCallback}>
       <label>
         First Name:
         <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
@@ -31,7 +47,7 @@ const Information = () => {
 
       <h2>Account information</h2>
       <Account email={email} password={password} />
-    </>
+    </Profiler>
   )
 }
 

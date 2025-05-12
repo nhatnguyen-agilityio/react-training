39c9d9da680984fd83e95c842a991074b5be55c1
Nhat Nnguyen <nhat.nguyen@asnet.com.vn>
2025-05-12 11:08:34 +0700

    feat(): create example with error boundary

---
+++ react-advanced-examples/src/components/ErrorBoundary/index.tsx
@@ -0,0 +1,34 @@
+import { Component, type ErrorInfo, type ReactNode } from "react";
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+
+interface ErrorBoundaryProps {
+  children: ReactNode;
+}
+
+class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
+    // Log the error to a service like Sentry
+    console.log("Error caught by ErrorBoundary", error, errorInfo);
+  }
+
+  render(): ReactNode {
+    if (this.state.hasError) {
+      return <h1>Something went wrong.</h1>;
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;

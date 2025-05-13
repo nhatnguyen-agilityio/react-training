763b2478b8ad0f86cf73989bc4bd79964cca3258
Nhat Nnguyen <nhat.nguyen@asnet.com.vn>
2025-05-13 15:56:11 +0700

    feat(): create example with custom hooks

---
+++ react-advanced-examples/src/components/Countdown/index.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import { useCountdown } from "../../Hooks/Countdown"
+
+const Countdown = () => {
+  const [timeInput, setTimeInput] = useState(0);
+
+  const timeLeft = useCountdown(timeInput);
+
+  return (
+    <>
+      <div>Countdown</div>
+      <label>
+        <input type="text" value={timeInput} onChange={(e) => setTimeInput(Number(e.target.value))}/>
+      </label>
+      <h1>The time left:{timeLeft} seconds</h1>
+    </>
+  )
+}
+
+export default Countdown

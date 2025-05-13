437b96935ecab1d2687d3cd12b954e071468e809
Nhat Nnguyen <nhat.nguyen@asnet.com.vn>
2025-05-13 09:30:58 +0700

    feat(): create example with input and form

--- react-advanced-examples/src/App.tsx
+++ react-advanced-examples/src/App.tsx
@@ -1,10 +1,10 @@
 import './App.css'
-import Information from './components/Information'
+import Form from './components/Form'
 
 function App() {
   return (
     <>
-      <Information />
+      <Form />
     </>
   )
 }

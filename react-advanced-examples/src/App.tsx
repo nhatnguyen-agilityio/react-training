8100e5f570caaba8ebb0bc845e55190f50e5ad5a
Nhat Nnguyen <nhat.nguyen@asnet.com.vn>
2025-05-26 14:25:06 +0700

    feat(): update component order in app file

--- react-advanced-examples/src/App.tsx
+++ react-advanced-examples/src/App.tsx
@@ -1,49 +1,31 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { AuthProvider } from './auth/AuthProvider'
-import Countdown from './components/Countdown'
-import Form from './components/Form'
-import MarkdownEditor from './components/MarkdownEditor'
-import Information from './components/Information'
-import Team from './components/Team'
-import Timer from './components/Timer'
 import ProtectedRoute from './components/ProtectedRoute'
-import './App.css'
 import Login from './pages/Login'
+import './App.css'
+import { lazy, Suspense } from 'react'
+import ErrorBoundary from './components/ErrorBoundary'
+
+const Home = lazy(() => import('./pages/Home'))
 
 function App() {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={
-            <ProtectedRoute>
-              <Countdown />
-            </ProtectedRoute>
-          } >
-            <Route index element={<MarkdownEditor />} />
-            <Route path="settings" element={<Information />} />
-          </Route>
-          <Route path="login" element= {<Login />} />
-          <Route path="form" element={<Form />} />
-          <Route element={<Countdown />}>
-            <Route path="information" element={<Information />} />
-            <Route path="markdown" element={<MarkdownEditor />} />
-          </Route>
-          <Route path="dashboard">
-            <Route path="settings" element={<Information />} />
-            <Route path="markdown" element={<MarkdownEditor />} />
-          </Route>
-          <Route path="teams">
-            <Route path="settings/:teamId" element={<Team />} />
-          </Route>
-          <Route path="user">
-            <Route path="settings/:userId?" element={<Information />} />
-          </Route>
-          <Route path="docs/*" element={<MarkdownEditor />} />
-          <Route path="timer" element={<Timer />} />
-        </Routes>
-      </BrowserRouter>
-    </AuthProvider>
+    <ErrorBoundary>
+      <AuthProvider>
+        <BrowserRouter>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              <Route path="home" element={
+                <ProtectedRoute>
+                  <Home />
+                </ProtectedRoute>
+              } />
+              <Route path="login" element={<Login />} />
+            </Routes>
+          </Suspense>
+        </BrowserRouter>
+      </AuthProvider>
+    </ErrorBoundary>
   )
 }
 

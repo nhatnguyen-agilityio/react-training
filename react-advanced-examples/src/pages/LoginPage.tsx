5f1490fb155bf8b8d081904fd1fedb9db754fcb0
Nhat Nnguyen <nhat.nguyen@asnet.com.vn>
2025-05-30 09:37:32 +0700

    feat(): fix build error

--- react-advanced-examples/src/pages/LoginPage.tsx
+++ react-advanced-examples/src/pages/LoginPage.tsx
@@ -14,7 +14,7 @@ const Login = () => {
 
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
-    const success = await auth.login(username, password);
+    const success = await auth.login(username, password, false);
     if (success) {
       navigate(fromLocation, { replace: true });
     } else {

bbdf2d78824f2c4c30a16dcc7ba23ce3379f26b8
Nhat Nnguyen <nhat.nguyen@asnet.com.vn>
2025-06-06 11:37:29 +0700

    feat(): add home button on not found screen

--- react-advanced-examples/src/components/NotFound/index.tsx
+++ react-advanced-examples/src/components/NotFound/index.tsx
@@ -1,5 +1,7 @@
 import notFoundImage from "@/assets/not-found.png"
 import Image from "../common/Image";
+import { Button } from "../ui/button";
+import { NavLink } from "react-router-dom";
 
 const NotFound = () => {
   return (
@@ -7,6 +9,10 @@ const NotFound = () => {
       <h2 className="text-6xl">OOPs</h2>
       <Image src={notFoundImage} alt="Not Found" className="w-100 h-auto" />
       <p className="text-6xl">404 - Page Not Found</p>
+      <NavLink to="/">
+        <Button className="bg-white w-25 h-12 mt-10 text-xl text-destructive border border-destructive rounded-sm hover:bg-destructive hover:text-background">Home</Button>
+      </NavLink>
+
     </div>
   );
 }

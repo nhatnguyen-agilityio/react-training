28e0adab6233546825f027229b7f616b022ba669
Nhat Nnguyen <nhat.nguyen@asnet.com.vn>
2025-09-09 16:40:35 +0700

    feat(): update env variable to work on stag

--- practice-one/web/furniture-store/src/constants/env-variables.ts
+++ practice-one/web/furniture-store/src/constants/env-variables.ts
@@ -1,4 +1,4 @@
 export const API_ENDPOINT =
-  import.meta.env.ENV === 'stag'
+  import.meta.env.VITE_ENV === 'stag'
     ? import.meta.env.VITE_STAG_API_URL
     : import.meta.env.VITE_LOCAL_API_URL;

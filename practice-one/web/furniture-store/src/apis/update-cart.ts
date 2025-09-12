6a6188eca091061dc69431a5323a21b914631d09
Nhat Nnguyen <nhat.nguyen@asnet.com.vn>
2025-09-12 15:15:42 +0700

    feat(): handle update cart item

---
+++ practice-one/web/furniture-store/src/apis/update-cart.ts
@@ -0,0 +1,58 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import type { CartInterface } from '../interfaces/cart';
+import { API_ENDPOINT } from '../constants/env-variables';
+import { API_ROUTES } from '../constants/api-routers';
+import { QUERY_KEY } from '../constants/query-keys';
+
+const updateCart = async (
+  cartId: number | string,
+  cartPayload: CartInterface,
+) => {
+  const url = new URL(`${API_ENDPOINT}${API_ROUTES.CARTS}/${cartId}`);
+
+  const res = await fetch(url, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(cartPayload),
+  });
+
+  if (!res.ok) {
+    let message = 'Failed to update cart';
+    try {
+      const data = await res.json();
+      if (data?.error) message = data.error;
+    } catch {
+      // ignore
+    }
+    throw new Error(message);
+  }
+
+  return res.json();
+};
+
+export const useUpdateCart = () => {
+  const queryClient = useQueryClient();
+
+  const { mutate, mutateAsync, isPending, error, ...rest } = useMutation<
+    boolean,
+    Error,
+    { cartId: number | string; cartPayload: CartInterface }
+  >({
+    mutationFn: ({ cartId, cartPayload }) => updateCart(cartId, cartPayload),
+    onSuccess: (_, variables) => {
+      queryClient.invalidateQueries({
+        queryKey: QUERY_KEY.USER_CART(variables.cartPayload.userId),
+      });
+    },
+  });
+
+  return {
+    mutate,
+    mutateAsync,
+    ...rest,
+    isLoading: isPending,
+    error: error?.message || '',
+  };
+};
--- practice-one/web/furniture-store/src/components/Cart/CartItem/index.tsx
+++ practice-one/web/furniture-store/src/components/Cart/CartItem/index.tsx
@@ -5,8 +5,11 @@ import { Input } from '../../ui/input';
 import { Skeleton } from '../../ui/skeleton';
 import { TriangleAlert } from 'lucide-react';
 import type { ProductVariant } from '../../../interfaces/products';
+import { useCallback, useEffect, useState } from 'react';
+import { useUpdateCart } from '../../../apis/update-cart';
 
 const CartItem = ({ cartItem }: { cartItem: CartInterface }) => {
+  const [quantityValue, setQuantityValue] = useState<number>(1);
   const productId = cartItem.items[0].productId;
   const variantId = cartItem.items[0].variantId;
 
@@ -15,6 +18,28 @@ const CartItem = ({ cartItem }: { cartItem: CartInterface }) => {
     isPending,
     isError,
   } = useGetProductDetail(String(productId) || '', !!productId);
+
+  const { mutate } = useUpdateCart();
+
+  useEffect(() => {
+    setQuantityValue(cartItem.items[0].quantity);
+  }, [cartItem]);
+
+  const handleChangeQuantity = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setQuantityValue(Number(e.target.value));
+
+      const updatedCart = {
+        ...cartItem,
+        items: cartItem.items.map((item, idx) =>
+          idx === 0 ? { ...item, quantity: Number(e.target.value) } : item,
+        ),
+      };
+
+      mutate({ cartId: cartItem.id || 0, cartPayload: updatedCart }, {});
+    },
+    [cartItem, mutate],
+  );
 
   if (isPending) {
     return (
@@ -91,14 +116,15 @@ const CartItem = ({ cartItem }: { cartItem: CartInterface }) => {
       </div>
       <div className="flex flex-col justify-between lg:items-end ml-3">
         <p className="text-app-primary font-semibold text-sm md:text:lg lg:text-xl">
-          {`$${productDetail.price * cartItem.items[0].quantity}`}
+          {`$${(productDetail.price * cartItem.items[0].quantity).toFixed(2)}`}
         </p>
         <div className="h-12 w-12 mr-4 lg:mr-0 flex items-end">
           <Input
             type="number"
             min={1}
             max={100}
-            defaultValue={1}
+            value={quantityValue}
+            onChange={handleChangeQuantity}
             className="bg-background-primary rounded-2xl p-0 text-center lg:pl-2"
           />
         </div>

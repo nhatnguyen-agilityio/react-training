34aa531fabcd58df976ccc9867212e4d272a9a25
Nhat Nnguyen <nhat.nguyen@asnet.com.vn>
2025-09-15 14:07:25 +0700

    feat(): implement storybook for image component

--- practice-one/web/furniture-store/.storybook/main.ts
+++ practice-one/web/furniture-store/.storybook/main.ts
@@ -1,16 +1,25 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    '@chromatic-com/storybook',
-    '@storybook/addon-docs',
     '@storybook/addon-a11y',
+    '@storybook/addon-docs',
     '@storybook/addon-vitest',
   ],
   framework: {
     name: '@storybook/react-vite',
     options: {},
   },
+  typescript: {
+    check: false,
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) =>
+        prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
+    },
+  },
 };
+
 export default config;
